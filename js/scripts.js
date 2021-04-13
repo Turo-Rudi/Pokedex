8fcfcc7a45a4f9525cc307232acea5515d8bb747
Turo-Rudi <jozsa.andrea@gmail.com>
2021-04-13 18:49:41 +0200

    Create Pokémon objects in pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,50 @@
-alert('Hello world');
+//Create a new variable called pokemonList and assign to it a blank array.
+let pokemonList = [];
 
-let favoriteFood = 'cakes';
-document.write(favoriteFood);
+//Add several objects to the array.
+pokemonList = [
+  {
+  //Charmander
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    gender: ['Male', 'Female'],
+    type: ['Fire'],
+  },
+
+  {
+  //Squirtle
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9,
+    gender: ['Male', 'Female'],
+    type: ['Water'],
+  },
+
+  {
+  //Caterpie
+    name: 'Caterpie',
+    height: 0.3,
+    weight: 2.9,
+    gender: ['Male', 'Female'],
+    type: ['Bug'],
+  },
+
+  {
+  //Rattata
+    name: 'Rattata',
+    height: 0.3,
+    weight: 3.5,
+    gender: ['Male', 'Female'],
+    type: ['Normal'],
+  },
+
+  {
+  //Weedle
+    name: 'Weedle',
+    height: 0.3,
+    weight: 3.2,
+    gender: ['Male', 'Female'],
+    type: ['Bug', 'Poison'],
+  },
+]
