419f682173732ac460e61ee75cfa772cfec3caa5
Turo-Rudi <jozsa.andrea@gmail.com>
2021-04-15 17:26:33 +0200

    loop pokemon names, height, biggest

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-//Create a new variable called pokemonList and assign to it a blank array.
-let pokemonList = [];
-
-//Add several objects to the array.
-pokemonList = [
+//Create a new variable called pokemonList and assign to it a blank array. Add several objects to the array.
+let pokemonList = [
   {
   //Charmander
     name: 'Charmander',
@@ -47,4 +44,13 @@ pokemonList = [
     gender: ['Male', 'Female'],
     type: ['Bug', 'Poison'],
   },
-]
+];
+
+//loop pokemonList names and height and display biggest
+for (var i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 0.5) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - That’s the biggest! ');
+  }else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+  }
+};
