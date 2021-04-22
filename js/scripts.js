4f1586ffd1904be24b8cdbeeeb276497c46c3a3d
Turo-Rudi <jozsa.andrea@gmail.com>
2021-04-22 14:54:35 +0200

    add addListItem() and eventListener()

--- js/scripts.js
+++ js/scripts.js
@@ -42,25 +42,58 @@ let pokemonRepository = (function() {
     },
   ];
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    button.innerText = pokemon.name;
+    button.classList.add('button');
+        // appended the button to li
+    listItem.appendChild(button);
+        // appended the button to ul
+    pokemonList.appendChild(listItem);
+        // EventListener for button
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   function getAll() {
     return pokemonList;
-    }
+  }
 
   function add(pokemon) {
-    if (typeof pokemon === 'object') {
-    pokemonList.push(pokemon);
-  }
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'weight' in pokemon &&
+      'gender' in pokemon &&
+      'type' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon not correct");
+    }
   }
 
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
 //forEach() loop IIFE pokemonList -at least- names and height
-let pokemonList = pokemonRepository.getAll();
+//let pokemonList = pokemonRepository.getAll();
 
-pokemonRepository.getAll().forEach(function(pokemonList) {
-  document.write('<h3>'+pokemonList.name+'</h3>' + ' [weight: ' + pokemonList.weight + '] ' + '<br>' + ' [height: ' + pokemonList.height + '] ');
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+  //document.write('<h3>'+pokemonList.name+'</h3>' + ' [weight: ' + pokemonList.weight + '] ' + '<br>' + ' [height: ' + pokemonList.height + '] ');
 });
